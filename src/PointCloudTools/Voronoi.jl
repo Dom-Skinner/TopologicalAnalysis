c9534acd745ca51f7408f8ee2e69f52c12ad6462
Dominic Skinner <dominic@dhcp-10-31-50-106.dyn.mit.edu>
2022-01-05 14:49:37 -0500

    Refactored Voronoi code

--- src/PointCloudTools/Voronoi.jl
+++ src/PointCloudTools/Voronoi.jl
@@ -13,29 +13,6 @@ function __init__()
     copy!(scipy_qhull, pyimport_conda("scipy.spatial.qhull", "scipy"))
 end
 
-
-function p_val(Positions)
-   p = zeros(length(Positions),length(Positions[1]))
-   for s = 1:length(Positions)
-       p[s,:] = Positions[s]
-   end
-   return p
-end
-
-function edge_find_voronoi(p,index_points,regions)
-   edge_index = []
-   for s = 1:length(p[:,1])
-        nbhd = regions[index_points[s]]
-        for s2 in nbhd
-            if s2 < 0
-               push!(edge_index,s)
-            end
-         end
-   end
-   unique!(edge_index)
-   return edge_index
-end
-
 function circumradius(e1,e2,e3)
     # see http://mathworld.wolfram.com/Circumradius.html
     a = sqrt(e1[1]^2+e1[2]^2)
@@ -55,23 +32,26 @@ function circumradius3D(e1,e2,e3,e4)
     Dz = det(e_mat[:,[1:3; 5]])
     c = det(e_mat[:,1:4])
     r = Dx^2+Dy^2+Dz^2-4*a*c
-    if r >= 0 
+    if r >= 0
     	return sqrt(r)/(2*abs(a))
     else
     	return Inf # infinite radius
     end
 end
 
-function alpha_shape2D!( α_val,simplices,p)
+function alpha_shape2D(simplices,p)
+    α_val = zeros(length(simplices[:,1]))
     for s = 1:length(simplices[:,1])
         e1 = p[simplices[s,1],:] - p[simplices[s,2],:]
         e2 = p[simplices[s,1],:] - p[simplices[s,3],:]
         e3 = p[simplices[s,2],:] - p[simplices[s,3],:]
         α_val[s] = circumradius(e1,e2,e3)
     end
+    return α_val
 end
 
-function alpha_shape3D!( α_val,simplices,p)
+function alpha_shape3D(simplices,p)
+    α_val = zeros(length(simplices[:,1]))
     for s = 1:length(simplices[:,1])
         e1 = p[simplices[s,1],:]
         e2 = p[simplices[s,2],:]
@@ -79,23 +59,61 @@ function alpha_shape3D!( α_val,simplices,p)
         e4 = p[simplices[s,4],:]
         α_val[s] = circumradius3D(e1,e2,e3,e4)
     end
+    return α_val
 end
 
-function alpha_shape_eval!(α_val,neighbours,simplices,p, α )
-
+function alpha_shape_eval(simplices,p)
     # This function performs an alpha shape analysis. It computes the radius of
-    # circumcircle/sphere for each simplex. If the radius is too large, all points
-    # are marked as boundary points.
-
-    # first compute the radius or α_val for each simplex
-
+    # circumcircle/sphere for each simplex.
     if size(p,2) == 3
-        alpha_shape3D!(α_val,simplices,p)
+        return alpha_shape3D(simplices,p)
     elseif size(p,2) == 2
-         alpha_shape2D!(α_val,simplices,p)
+        return alpha_shape2D(simplices,p)
     end
+end
 
-    # select α if not specified
+
+function edge_indices_delaunay(simplices,neighbours)
+    # Identify points which are edge points.
+    edge_index = Array{Int64}(undef, 0)
+    for i = 1:size(simplices,1), j = 1:size(simplices,2)
+            if neighbours[i,j]< 1
+                push!(edge_index,simplices[i,setdiff(1:4,j)]...)
+            end
+    end
+    unique!(edge_index)
+    return edge_index
+end
+
+
+# Convert vector of vectors to matrix if not matrix already
+p_vals(P::Vector) = permutedims(hcat(P...))
+p_vals(P::Matrix) = P
+
+
+function neighbor_relabel(x::Int64,α_keep::Vector{Int64})
+    if x == 0
+        return x
+    else
+        return α_keep[x]
+    end
+end
+
+function Delaunay_find(Positions,α)
+    p = p_vals(Positions)
+    tri = scipy_qhull.Delaunay(p)
+
+    simplices = tri.simplices.+1 # convert to julia indexing
+    neighbours = tri.neighbors.+1
+    # simplices contains the triangles/tetrahedrons which form the Delaunay
+    # neighbours tells you which of the triangles/tetrahedrons border each other
+    # If they border the edge, they border 0
+
+
+    # Compute the α value of each simplex
+    α_val = alpha_shape_eval(simplices,p)
+
+    # Determine α threshold value
     if α == 0
         α = 2*median(α_val)
         println("using default alpha of 2*(median alpha val) = ",α)
@@ -103,57 +121,29 @@ function alpha_shape_eval!(α_val,neighbours,simplices,p, α )
         println("using custom alpha = ",α)
     end
 
-    # For simplices that are too large, mark them as edge points
-    for s in 1:length(neighbours)
-        if neighbours[s] > 0
-            if  α_val[neighbours[s]] > α
-                neighbours[s] = -1.
-            end
+    # Figure out which simplices we want to keep, and also keep track of their
+    # new indices in order to update the neighbour vector
+    α_keep = zeros(Int64,length(α_val))
+    counter = 1
+    for i = 1:length(α_keep)
+        if α_val[i] < α
+            α_keep[i] = counter
+            counter = counter + 1
+        else
+            α_keep[i] = 0
         end
     end
+    # Relabel points in neighbours (some simplices are now edge points)
+    f = x->neighbor_relabel(x,α_keep)
+    neighbours = f.(neighbours)
+    # Only keep the small simplices
+    simplices = simplices[α_keep.>0,:]
+    neighbours = neighbours[α_keep.>0,:]
 
-    α_val .= α_val .< α
-
-end
-
-function edge_indices_delaunay(simplices,neighbours)
-    # Identify points which are edge points.
-    edge_index = Array{Int64}(undef, 0)
-    for s = 1:length(simplices[:,1])
-        for s2 = 1:length(simplices[1,:])
-            if neighbours[s,s2]< 1
-                 for s3 = 1:length(simplices[1,:])
-                 # including opposite points
-                 #push!(edge_index,simplices[s,s3])
-                 # not including opposite points
-                     if s2 != s3
-                        push!(edge_index,simplices[s,s3])
-                     end
-                 end
-            end
-        end
-    end
-    unique!(edge_index)
-    return edge_index
-end
-
-
-function Delaunay_find(Positions,α)
-    p = p_val(Positions)
-    tri = scipy_qhull.Delaunay(p)
-
-    simplices = tri.simplices.+1 # convert to julia indexing
-    neighbours = tri.neighbors.+1
-    # simplices contains a bunch of triangles/tetrahedrons which form the Delaunay
-    # neighbours tells you which of the triangles/tetrahedrons border each other
-    # If they border the edge, they border -1
-
-    α_val = zeros(length(simplices[:,1]))
-    alpha_shape_eval!(α_val,neighbours,simplices,p,α)
-
+    # This function keeps track of which points are on the boundary
     edge_index = edge_indices_delaunay(simplices,neighbours)
 
-    return p, simplices, neighbours, edge_index, α_val
+    return p, simplices, neighbours, edge_index, α_val,α
 end
 
 
@@ -201,7 +191,9 @@ function find_delaunay_network_2D(Positions, path_out, periodic, α, tol)
     if periodic; periodic_extend!(Positions,tol=tol); end
 
     # Find the Delaunay and construct the full graph
-    p, simplices, neighbrs, edge_index = Delaunay_find(Positions, α)
+    p, simplices, neighbrs, α_val, α = Delaunay_find(Positions, α)
+    edge_index = α_val .< α
+
     g_full = graph_construct(simplices,length(Positions))
 
     savegraph(path_out*"_graph.lgz",g_full)
@@ -223,17 +215,15 @@ function find_delaunay_network_2D(Positions, path_out, periodic, α, tol)
 end
 
 
-
 function find_delaunay_network_3D(Positions, path_out, periodic, α, tol, edge_keep)
 
     N = length(Positions)
     if periodic; periodic_extend!(Positions,tol=tol); end
 
-    p, simplices, neighbrs, edge_index, α_val = Delaunay_find(Positions, α)
+    p, simplices, neighbrs, edge_index, α_val, α = Delaunay_find(Positions, α)
 
     if edge_keep
-        not_edge = [i for i in 1:size(p,1)] # keep all indices
-        simplices = simplices[α_val .== 1,:] # but only keep simplices that have alpha val < α
+        not_edge = Vector(1:size(p,1)) # keep all indices
     else
         not_edge = setdiff(1:size(p,1), edge_index)
     end
