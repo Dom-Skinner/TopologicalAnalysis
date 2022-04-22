4232adb418d414e0fec2c4b0e2f672876bf28635
Dominic Skinner <dom15@live.com>
2022-04-22 16:55:53 -0400

    updating perturbation calc - still working on it

--- src/DistanceComputeTools.jl
+++ src/DistanceComputeTools.jl
@@ -194,7 +194,7 @@ end
 
 
 
-function CFTD_perturbation_0(g,p0,p1,r1;k=10)
+function CFTD_perturbation_0(g,p0,p1,r1)
 
     # collect edges
     es = collect(lg.edges(g))
@@ -211,62 +211,90 @@ function CFTD_perturbation_0(g,p0,p1,r1;k=10)
     model = Model(Gurobi.Optimizer)
 	#set_optimizer_attributes(model, "OptimalityTol" => 1e-7)
 	#set_optimizer_attributes(model, "FeasibilityTol" => 1e-7)
-    @variable(model,q1[1:num_v,1:k+1] )
-    @variable(model,F0[1:num_e,1:k] >= 0)
+
+    @variable(model,J1[1:num_e] >= 0)
 
     # rewrite the objective as linear, with SOC constraints
-    @objective(model,Min,sum(F0[j,i]^2 *(1/p0[e_tot[j,1]] + 1/p0[e_tot[j,2]])/2
-			for i=1:k for j = 1:num_e ))
+    @objective(model,Min,sum(J1[j]^2 *(1/p0[e_tot[j,1]] + 1/p0[e_tot[j,2]])/2
+			for j = 1:num_e ))
 
-    @constraint(model,q1[:,1] .== r1)
-    @constraint(model,q1[:,end] .== p1)
-
-    for i = 1:k , j in 1:num_v
-    @constraint(model,-sum(F0[findall(e_tot[:,1] .== j),i]) +
-        sum(F0[findall(e_tot[:,2] .== j),i]) == q1[j,i+1]-q1[j,i] )
+    for j in 1:num_v
+	    @constraint(model,-sum(J1[findall(e_tot[:,1] .== j)]) +
+	        sum(J1[findall(e_tot[:,2] .== j)]) == p1[j]  - r1[j])
     end
 
     optimize!(model)
 
-	F0_used = zeros(lg.ne(g),k)
-	edge_used = [maximum(value.(F0[i,:])) > maximum(value.(F0[i+lg.ne(g),:])) for i = 1:lg.ne(g)]
+	# After optimizing, only keep the edges that were actually used
+	J1_used = zeros(lg.ne(g))
+	edge_used = [maximum(value.(J1[i])) > maximum(value.(J1[i+lg.ne(g)])) for i = 1:lg.ne(g)]
 	edge_used = vcat(edge_used, .!edge_used)
+	J1_used = value.(J1[edge_used])
 
-	F0_used = value.(F0[edge_used,:])
-
-    return e_tot[edge_used,:],F0_used,value.(q1), sqrt(k*objective_value(model))
+    return e_tot[edge_used,:],J1_used, objective_value(model)
 end
 
-
-
-function CFTD_perturbation_1(e,p0,F0,q1,p2,r2;k=10)
+function CFTD_perturbation_1(e,p0,J1,p1,r1,p2,r2)
 
     # collect edges
 	num_v = length(unique(e))
 	num_e = size(e,1)
 
+	I = [e[:,1];e[:,2]]
+	J = [1:size(e,1);1:size(e,1)]
+	V = [-1*ones(size(e,1));ones(size(e,1))]
+	Dt = sparse(I,J,V)
 
-    # With graph specified, now set up the convex problem
-    model = Model(Gurobi.Optimizer)
-	#set_optimizer_attributes(model, "OptimalityTol" => 1e-7)
-	#set_optimizer_attributes(model, "FeasibilityTol" => 1e-7)
-    @variable(model,q2[1:num_v,1:k+1] )
-    @variable(model,F1[1:num_e,1:k])
+	if maximum(abs.(p2 .- r2))<1e-10
+		J2 = zeros(num_e)
+	else
+		J2 = lsmr(Dt, p2 .- r2)
+	end
+	#F1 = Dt \ (p2 .- r2)
 
-    @objective(model,Min,sum(F1[j,i]*F0[j,i] *(1/p0[e[j,1]] + 1/p0[e[j,2]]) +
-			F0[j,i]^2 *(-q1[e[j,1],i]/p0[e[j,1]]^2 -q1[e[j,2],i+1]/p0[e[j,2]]^2)/2
-			for i=1:k for j = 1:num_e ))
+	chi = sum(  J2[i]*J1[i]*( 1/ p0[e[i,1]] + 1/p0[e[i,2]]) for i in 1:num_e)
 
-    @constraint(model,q2[:,1] .== r2)
-    @constraint(model,q2[:,end] .== p2)
+	lambda = -0.25*sum( J1[i]^2 * ( (p1[e[i,1]] +r1[e[i,1]])/p0[e[i,1]]^2 +
+		(p1[e[i,2]] +r1[e[i,2]])/p0[e[i,2]]^2) for i in 1:num_e)
+	I1 = chi+lambda
+    return chi, I1
+end
 
-    for i = 1:k , j in 1:num_v
-    @constraint(model,-sum(F1[findall(e[:,1] .== j),i]) +
-        sum(F1[findall(e[:,2] .== j),i]) == q2[j,i+1]-q2[j,i] )
+
+function CFTD_perturbation_2(e,p0,J1,p1,r1,p2,r2,nu2;k=10)
+
+	@assert size(e,1) == length(J1)
+
+	num_e = size(e,1)
+	verts = unique(e)
+	num_v = length(verts)
+	verts_inv = Dict(verts .=> 1:num_v)
+
+	model = Model(Gurobi.Optimizer)
+
+	@variable(model,q2[1:num_v,1:k+1])
+	@variable(model,J2[1:num_e,1:k]>=-Inf)
+
+
+    @constraint(model,q2[:,1] .== p2[verts])
+    @constraint(model,q2[:,end] .== r2[verts])
+
+    for i = 1:k , j in verts
+    @constraint(model,-sum(J2[findall(e[:,1] .== j),i]) +
+        sum(J2[findall(e[:,2] .== j),i]) == q2[verts_inv[j],i+1]-q2[verts_inv[j],i] )
     end
-	#return model
-    optimize!(model)
+
+	#nu = [J1[i]*( 1/ p0[e[i,1]] + 1/p0[e[i,2]]) for i = 1:num_e]
+	#@constraint(model, sum( sum(J2[j,:])*nu[j] for j = 1:num_e) == chi)
 
 
-    return value.(F1), value.(q2), k*objective_value(model)
+
+	@objective(model,Min,sum(J2[j,i]*nu2[j,i] for i = 1:k,j = 1:num_e) +
+			0.5*k*sum(J2[j,i]^2 *( 1/ p0[e[j,1]] + 1/p0[e[j,2]])  for i = 1:k,j = 1:num_e) -
+			(0.5/k)*sum(J1[j]^2 *( q2[e[j,1],i+1]/ p0[e[j,1]]^2 + q2[e[j,2],i]/p0[e[j,2]]^2)  for i = 1:k,j = 1:num_e))
+
+	println(model)
+	optimize!(model)
+
+    return objective_value(model)
 end
