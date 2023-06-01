0b253e2b72b58c0ffe148f843e23ae59aa8059ce
Dominic Skinner <dominic@dhcp-10-105-153-172.wireless.northwestern.private>
2023-06-01 15:25:25 -0500

    updated example scripts for 3D

--- examples/Example_3D.jl
+++ examples/Example_3D.jl
@@ -1,15 +1,14 @@
-# Example script showing how to use the package LocalCellularStructure
+# Example script showing how to use the package LocalCellularStructure in 3D.
 
-# First load the required packages. For instructions on how to install a custom package see, https://syl1.gitbook.io/julia-language-a-concise-tutorial/language-core/11-developing-julia-packages
 using LocalCellularStructure
 using Random
 
 
 save_directory = "./"
 
-# First let us create some data with some data, say random points in 2D.
+# First let us create some data with some data, say random points in 3D.
 Random.seed!(1234)
-Positions = rand(1000,2) # 1000 random points.
+Positions = rand(1000,3) # 1000 random points.
 
 # First we wish to compute the Delaunay. The function below returns a structure containing
 # all information relevant to the delaunay triangulation.
@@ -18,37 +17,37 @@ Positions = rand(1000,2) # 1000 random points.
 delaunay_info = find_delaunay_network(Positions, periodic=false, alpha = 0)
 
 # delaunay_info is a structure of (custom) type TopologicalNetwork.
-# delaunay_info.simplices is a N×3 list of vertices making up the N triangles of the delauany.
+# delaunay_info.simplices is a N×4 list of vertices making up the N tetrahedrons of the delauany.
 # delaunay_info.not_edge are the vertices identified as interior points (can be useful to plot
 #   to make sure the alpha value has been correctly chosen).
 # There are other fields, which we can ignore for now.
 
 # one can save a TopologicalNetwork type as a HDF5 file by calling
-save(save_directory*"test_delaunay_network.h5",delaunay_info)
+save(save_directory*"test_delaunay_network_3D.h5",delaunay_info)
 
 # and one can load it again by
-delaunay_info_reloaded = load(save_directory*"test_delaunay_network.h5")
+delaunay_info_reloaded = load(save_directory*"test_delaunay_network_3D.h5")
 
 # Next, we compute the motifs for a network, using the function compute_motifs
 motif_array = compute_motifs(delaunay_info)
 
 # motif_array is of type MotifArray
 # motif_array.idx contains the original vertex number, and motif_array.tvec 
-# contains the corresponding weinberg vector.
+# contains the corresponding topological vector.
 
 # we can save a MotifArray, much as we can save a TopologicalNetwork
-save(save_directory*"motif_array.h5",motif_array)
+save(save_directory*"motif_array_3D.h5",motif_array)
 
 # and load it again by
-motif_array_reloaded = load(save_directory*"motif_array.h5")
+motif_array_reloaded = load(save_directory*"motif_array_3D.h5")
 
-# a MotifArray is associated with a particular Delauany triangulation.
+# a MotifArray is associated with a particular Delauany tessellation.
 # suppose we want to combine multiple MotifArray's to get a motif distribution
 # In this case call avg_motif which takes any number of arguments 
 # (pretend for now motif_array and motif_array_reloaded are different)
 motif_distribution = avg_motif(motif_array,motif_array_reloaded)
 # This returns a type of MotifDist, where MotifDist.map is a dictionary 
-# taking a weinberg vector to a frequency. MotifDist's can be saved in much the same way as before
+# taking a topological vector to a frequency. MotifDist's can be saved in much the same way as before
 
 
 # Now on to computing the flip graph. Pass in a MotifDist, or any number of MotifArray's, and
@@ -61,14 +60,14 @@ flip_graph = compute_flip(motif_distribution; restrict = 0,thresh=0.0)
 # according to the page rank algorithm. The lower the threshold the more vertices are kept.
 
 # flip graphs can be saved/loaded as before.
-save(save_directory*"flip_graph.h5",flip_graph)
+save(save_directory*"flip_graph_3D.h5",flip_graph)
 
 
 # Now we are ready to compute a distance between two MotifDist's or two MotifArray's
 # First stack them into a single vector
 stacked_motif_array = vcat(motif_array,motif_array_reloaded)
 # Then call the distance calculation
-d = calculate_distance_matrix(flip_graph, stacked_motif_array, optimal_transport=false) # N.B. optimal transport set to false since it requires Gurobi license
+d = calculate_distance_matrix(flip_graph, stacked_motif_array, optimal_transport=false)
 # as expected d is a matrix of zeros, because motif_array and motif_array_reloaded are the same thing.
 
 
@@ -76,15 +75,15 @@ d = calculate_distance_matrix(flip_graph, stacked_motif_array, optimal_transport
 # We will take 5 realizations of a random distribution of points, and 5 realizations of a perturbed grid
 function generate_random_pts()
     # generates a MotifArray for a random distribution of points
-    pos = rand(1000,2)
+    pos = rand(3375,3)
     delaunay_inf = find_delaunay_network(pos, periodic=false, alpha = 0)
     return compute_motifs(delaunay_inf)
 end
 
 function generate_perturbed_grid()
     # generates a MotifArray for a perturbed grid of points
-    pos = [[x,y] for x in range(0,1,31), y in range(0,1,31)][:]
-    pos .+= [0.01*randn(2) for i = 1:length(pos)]
+    pos = [[x,y,z] for x in range(0,1,15), y in range(0,1,15), z in range(0,1,15)][:]
+    pos .+= [0.01*randn(3) for i = 1:length(pos)]
     delaunay_inf = find_delaunay_network(pos, periodic=false, alpha = 0)
     return compute_motifs(delaunay_inf)
 end
@@ -115,3 +114,23 @@ MDS_coords = MultivariateStats.transform(MultivariateStats.fit(MDS,d, maxoutdim=
 p = scatter(MDS_coords[1,1:5],MDS_coords[2,1:5],label="Random points",
     xlabel="MDS PC 1", ylabel="MDS PC 2",grid=false,aspect_ratio=true)
 scatter!(p,MDS_coords[1,6:10],MDS_coords[2,6:10],label="Perturbed grid")
+
+
+######
+# The distance matrix computation can be run in parallel, with each element of 
+# the matrix able to be computed independently. The parallized code is contained within
+# the package. All the user has to do is add some number of processes to their julia script.
+# For example
+using Distributed # for addprocs
+
+addprocs(3) # add, say, 3 processes
+println(nworkers()) # check they have been added
+
+@everywhere using LocalCellularStructure # make sure all processes are aware of LocalCellularStructure
+
+
+flip_graph_in = save_directory*"flip_graph_3D.h5"
+motif_in = [save_directory*"motif_array_3D.h5",save_directory*"motif_array_3D.h5",save_directory*"motif_array_3D.h5"] # e.g., some array containing paths to saved motifs
+fg = load(flip_graph_in)
+motif_arr = load.(motif_in)
+d = calculate_distance_matrix(fg, motif_arr, optimal_transport=false)
